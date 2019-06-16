af6e2b3972416f227753ee9a9540467723ea2a36
chima2g <chima2g@hotmail.com>
2019-06-16 22:23:19 +0100

    spaced ArticleCard footer elements

--- src/Article/ArticleCard.js
+++ src/Article/ArticleCard.js
@@ -5,15 +5,19 @@ const ArticleCard = ({ article }) => {
   return (
     <div className="mt-3">
       <Card key={article.article_id} className="mt-5">
-        <Card.Header as="h5">{article.title}</Card.Header>
         <Card.Body>
+          <h3 className="mr-3">{article.title}</h3>
           <p>{article.body}</p>
           <blockquote className="blockquote mb-0">
             <footer className="blockquote-footer">
-              Author: {article.author}, Topic: {article.topic}, Article ID:{" "}
-              {article.article_id}, Created:{" "}
-              {new Date(article.created_at).toLocaleString()}, Votes:{" "}
-              {article.votes}, Comments: {article.comment_count}
+              <label className="mr-3">Author: {article.author}</label>
+              <label className="mr-3">Topic: {article.topic}</label>
+              <label className="mr-3">Article ID: {article.article_id}</label>
+              <label className="mr-3">
+                Created: {new Date(article.created_at).toLocaleString()}
+              </label>
+              <label className="mr-3">Votes: {article.votes}</label>
+              <label className="mr-3">Comments: {article.comment_count}</label>
             </footer>
           </blockquote>
         </Card.Body>
